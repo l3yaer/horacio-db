0dd8cade77678e9aaa0021211fb4ff568963799a
Yago Teixeira <yagaot@gmail.com>
2019-11-06 16:45:49 -0300

    insert em tabela fixa

--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 #include "input.h"
 
 int main() {
-  InputBuffer * input_buffer = input_buffer_new();
+  InputBuffer* input_buffer = input_buffer_new();
+  Table* table = new_table();
   while(true) {
     print_prompt();
     input_buffer_read(input_buffer);
-
 
     if(input_buffer->buffer[0] == '.'){
       switch(do_meta_command(input_buffer)) {
@@ -27,12 +27,21 @@ int main() {
     switch (prepare_statement(input_buffer, &statement)) {
       case PREPARE_SUCCESS:
         break;
+      case PREPARE_SYNTAX_ERROR:
+        printf("Syntax error. Could not parse statement.\n");
+        continue;
       case PREPARE_UNRECOGNIZED_STATEMENT:
         printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
         continue;
     }
 
-    execute_statement(&statement);
-    printf("Executed\n");
+    switch (execute_statement(&statement, table)) {
+      case EXECUTE_SUCCESS:
+        printf("Executed. \n");
+        break;
+      case EXECUTE_TABLE_FULL:
+        printf("Error: Table full\n");
+        break;
+    }
   }
 }
--- statement.h
+++ statement.h
@@ -20,11 +20,46 @@ typedef enum {
   STATEMENT_SELECT
 } StatementType;
 
+#define size_of_attribute(Struct,Attribute) sizeof(((Struct*)0)->Attribute)
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
   StatementType type;
+  Row row_to_insert;
 } Statement;
 
 
+#define TABLE_MAX_PAGES 100
+
+typedef struct {
+  uint32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+} Table;
+
+Table* new_table();
+void free_table(Table* table);
+
 PrepareResult prepare_statement(struct input_buffer* input_buffer, Statement* statement);
-void execute_statement(Statement* statement);
+
+void serialize_row(Row* source, void* destination);
+void deserialize_row(void* source, Row* destination);
+void print_row(Row* row);
+void* row_slot(Table* table, uint32_t row_num);
+
+typedef enum {
+  EXECUTE_TABLE_FULL,
+  EXECUTE_SUCCESS
+} ExecuteResult;
+
+ExecuteResult execute_statement(Statement* statement, Table* table);
+ExecuteResult execute_insert(Statement* statement, Table* table);
+ExecuteResult execute_select(Statement* statement, Table* table);
 #endif //HORACIODB_STATEMENT_H
