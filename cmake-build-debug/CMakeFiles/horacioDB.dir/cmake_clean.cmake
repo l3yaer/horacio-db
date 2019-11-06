ed47a30c01642cbc2e956cf6b49faf9bed3a8f52
Yago Teixeira <yagaot@gmail.com>
2019-11-06 13:52:49 -0300

    prompt basico

---
+++ cmake-build-debug/CMakeFiles/horacioDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/horacioDB.dir/main.c.o"
+  "horacioDB.pdb"
+  "horacioDB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/horacioDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
